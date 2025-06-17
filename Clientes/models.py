37ac9b447aa5a83d123e9b90bf8cc15e50922be3
Ferghit <jf4516768@gmail.com>
2025-06-16 21:31:26 -0500

    cambio 2

--- Clientes/models.py
+++ Clientes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    idCliente=models.PositiveIntegerField(unique=True) 
+    apellidos=models.CharField(max_length=80)
+    nombres=models.CharField(max_length=80) 
+    direccion=models.CharField(max_length=100) 
+    sexo=models.CharField(max_length=1)
+    estado=models.BooleanField() 
+    def __str__(self): return self.idCategoria
