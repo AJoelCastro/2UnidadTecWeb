25839b5177841931066032ee1d753fd82f2545cb
AJoelCastro <misthorn33@gmail.com>
2025-07-04 13:42:00 -0500

    changes 2

--- Clientes/models.py
+++ Clientes/models.py
@@ -5,7 +5,7 @@ class Cliente(models.Model):
     
     nombres=models.CharField(max_length=80)
     ruc_dni= models.CharField(max_length=11, unique=True)
-    email= models.EmailField(blank=True, null=True) 
+    email= models.EmailField(max_length=100,blank=True, null=True) 
     direccion=models.CharField(max_length=100) 
 
     def __str__(self): return self.nombres
